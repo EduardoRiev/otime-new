e6c5ce60eb8b115dccaffb3ff221e0308255f36b
Rafael Horacio <rafael.horacio@escolar.ifrn.edu.br>
2021-12-21 17:38:38 -0300

    Adiciona o componente v-dialog para excluir o professor em Professore.vue

--- frontend/src/components/Professores.vue
+++ frontend/src/components/Professores.vue
@@ -29,9 +29,40 @@
         </v-card-title>
         <v-card-actions>
           <v-btn outlined text> <v-icon small>mdi-pencil</v-icon>editar </v-btn>
-          <v-btn outlined text>
-            <v-icon small>mdi-delete</v-icon>remover
-          </v-btn>
+          <!------------------------------------------------REMOVER------------------------------------------------>
+          <template>
+            <v-col cols="auto">
+              <v-dialog max-width="600">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn text outlined v-bind="attrs" v-on="on">
+                    <v-icon small>mdi-delete</v-icon>
+                    REMOVER
+                  </v-btn>
+                </template>
+                <template v-slot:default="dialog2">
+                  <v-card>
+                    <v-card-title class="text-h5 red white--text lighten-2"
+                      >EXCLUIR</v-card-title
+                    ><v-card-text></v-card-text>
+                    <v-card-text class="text-md-body-1 black--text">
+                      Deseja remover ? {{ professor.nome }}
+                    </v-card-text>
+                    <v-divider></v-divider>
+                    <v-card-actions class="justify-end">
+                      <v-spacer></v-spacer>
+                      <v-btn text @click="dialog2.value = false">voltar</v-btn>
+                      <v-btn
+                        color="error"
+                        @click="deleteProfessor(professor.id)"
+                        >REMOVER</v-btn
+                      >
+                    </v-card-actions>
+                  </v-card>
+                </template>
+              </v-dialog>
+            </v-col>
+          </template>
+          <!------------------------------------------------FIM-REMOVER--------------------------------------------->
           <v-btn outlined text>
             <v-icon small>mdi-format-list-bulleted-square</v-icon>detalhes
           </v-btn>
@@ -129,7 +160,10 @@ export default {
       };
       this.axios
         .post("http://otime-api.herokuapp.com/professores/", professor)
-        .then((response) => (this.professores = [...this.professores, response.data]))
+        .then(
+          (response) =>
+            (this.professores = [...this.professores, response.data])
+        )
         .catch((error) => console.log(error));
       this.dialog = false;
       this.professor.nome = null;
@@ -137,7 +171,7 @@ export default {
       this.professor.coordenador = null;
       this.professor.imagem = null;
     },
-   deleteProfessor(professorId) {
+    deleteProfessor(professorId) {
       this.axios
         .delete("http://otime-api.herokuapp.com/professores/" + professorId)
         .then(() => {
@@ -145,7 +179,7 @@ export default {
             (p) => p.id != professorId
           );
         });
-    }, 
+    },
   },
 };
 </script>
