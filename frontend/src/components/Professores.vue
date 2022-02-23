4a056c351e6e28208df7a3033271936db5da8ded
Rafael Horacio <rafael.horacio@escolar.ifrn.edu.br>
2022-02-22 23:00:50 -0300

    Adiciona modal para permitir a edição de dados dos professores

--- frontend/src/components/Professores.vue
+++ frontend/src/components/Professores.vue
@@ -28,7 +28,57 @@
           ></v-simple-checkbox>
         </v-card-title>
         <v-card-actions>
-          <v-btn outlined text> <v-icon small>mdi-pencil</v-icon>editar </v-btn>
+        <!------------------------------------------------EDITAR------------------------------------------------>
+          <template>
+            <v-col cols="auto">
+              <v-dialog max-width="600">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn text outlined v-bind="attrs" v-on="on">
+                    <v-icon small>mdi-pencil</v-icon>
+                    EDITAR
+                  </v-btn>
+                </template>
+                <template v-slot:default="dialog3">
+                  <v-card>
+                    <v-card-title class="headline"
+                      >EDITAR PROFESSOR</v-card-title
+                    >
+                    <v-card-text>
+                      <v-container>
+                        <v-form ref="form" v-model="isValid">
+                          <v-text-field
+                            required
+                            label="nome"
+                            v-model="professor.nome"
+                          ></v-text-field>
+                          <v-text-field
+                            required
+                            label="abreviatura"
+                            v-model="professor.abreviatura"
+                          ></v-text-field>
+                          <v-checkbox
+                            label="coordenador"
+                            v-model="professor.coordenador"
+                          ></v-checkbox>
+                        </v-form>
+                      </v-container>
+                    </v-card-text>
+                    <v-spacer></v-spacer>
+                    <v-card-actions class="justify-end">
+                      <v-spacer></v-spacer>
+                      <v-btn text @click="dialog3.value = false">VOLTAR</v-btn>
+                      <v-btn
+                        color="success"
+                        @click="atualizarProfessor(professor.id, professor)"
+                        >ATUALIZAR</v-btn
+                      >
+                    </v-card-actions>
+                  </v-card>
+                </template>
+              </v-dialog>
+            </v-col>
+          </template>
+          <!------------------------------------------------FIM-EDITAR--------------------------------------------->
           <!------------------------------------------------REMOVER------------------------------------------------>
           <template>
             <v-col cols="auto">
